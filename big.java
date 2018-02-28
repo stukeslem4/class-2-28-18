035df658506ea2a6f76cedbcc6aa4669d4521fac
Lemon <s.lemon.2016@mymail.jcsu.edu>
2018-02-28 13:25:06 -0500

    first commit

---
+++ big.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class big {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Scanner V=new Scanner(System.in);	
+	System.out.println("Enter in a word");	
+	String U=V.nextLine();
+	
+	char T=U.charAt(0);
+		System.out.println(T);
+	int a1=U.length()-2;	
+		
+	System.out.println(U.charAt(a1));	
+		
+	}
+
+}
---
+++ character.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class character {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	Scanner Y=new Scanner(System.in);
+	System.out.println("Enter in a word");	
+	String B=Y.nextLine();	
+		
+	int l3=B.length()-1;	
+		
+	System.out.println(B.charAt(l3));	
+		
+		
+	}
+
+}
